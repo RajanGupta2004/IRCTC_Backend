18725602e6df0ce586e601c05308bd7407e6ff47
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2026-05-11 00:38:37 +0530

    application is containerise

--- user-service/src/middleware/error.middleware.ts
+++ user-service/src/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import logger from "../config/logger";
+import { AppError } from "../utils/AppError";
 
 export const errorHandler = (
   error: any,
@@ -13,6 +14,13 @@ export const errorHandler = (
     url: req.originalUrl,
     method: req.method,
   });
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
   res.status(error.statusCode || 500).json({
     success: false,
     message: error.message || "Internal Server Error",
